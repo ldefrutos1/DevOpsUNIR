d9e62209677d6d9fb9ac55901720f80b1c14142b
Luis de Frutos Redondo <luis.defrutos@ineco.com>
2020-12-25 01:06:34 +0100

    Cambiamos a java 8 y se añade un text

---
+++ src/test/java/hello/TestingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
